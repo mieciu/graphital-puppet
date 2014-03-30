eed199b68c4a8722d61fb536c9d99474c16fff24
mieciu <przemyslaw.hejman@gmail.com>
2014-03-30 01:43:28 +0100

    Update service.pp

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,5 +1,9 @@
 class graphital::service inherits graphital { 
 
+  # @TODO -> This is the place, where depends on deamonizing method
+  #          chosen by user, proper things will be set
+  #          (like running with ruby-deamonize rather than upstart)
+
   service{ 'graphital':
     ensure => "running",
     provider => "upstart",
